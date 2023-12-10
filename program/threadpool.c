bbb7a40e4f20fa053c152f5c9ab3b750a4104b63
Данил Долгов <spectrefromthefuture@gmail.com>
2023-12-10 21:27:55 +0400

    looks complete

--- program/threadpool.c
+++ program/threadpool.c
@@ -1,82 +1,101 @@
-/**
- * Implementation of thread pool.
- */
-
-#include <pthread.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <semaphore.h>
 #include "threadpool.h"
 
-#define QUEUE_SIZE 10
+#define QUEUE_SIZE 20
 #define NUMBER_OF_THREADS 3
 
-#define TRUE 1
+pthread_t bee; // the worker bee
+threadpool pool;
 
-// this represents work that has to be 
-// completed by a thread in the pool
-typedef struct 
-{
-    void (*function)(void *p);
-    void *data;
-}
-task;
+// initialize the thread pool
+void pool_init(void) {
+    // Инициализация структуры пула потоков и выделение памяти
+    pool.threads = (pthread_t*)malloc(sizeof(pthread_t) * NUMBER_OF_THREADS);
+    if (pool.threads == NULL) {
+        perror("Error allocating memory for threads");
+        exit(EXIT_FAILURE);
+    }
 
-// the work queue
-task worktodo;
+    initialize_queue(&pool.queue);
 
-// the worker bee
-pthread_t bee;
+    // Инициализация мьютекса
+    if (pthread_mutex_init(&pool.mutex, NULL) != 0) {
+        perror("Error initializing mutex");
+        exit(EXIT_FAILURE);
+    }
 
-// insert a task into the queue
-// returns 0 if successful or 1 otherwise, 
-int enqueue(task t) 
-{
-    return 0;
-}
+    // Инициализация семафора
+    if (sem_init(&pool.sem, 0, 0) != 0) {
+        perror("Error initializing semaphore");
+        exit(EXIT_FAILURE);
+    }
 
-// remove a task from the queue
-task dequeue() 
-{
-    return worktodo;
+    // Создание потоков
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
+        if (pthread_create(&pool.threads[i], NULL, worker, NULL) != 0) {
+            perror("Error creating thread");
+            exit(EXIT_FAILURE);
+        }
+    }
 }
 
 // the worker thread in the thread pool
-void *worker(void *param)
-{
-    // execute the task
-    execute(worktodo.function, worktodo.data);
+void* worker(void* param) {
+    while (1) {
+        sem_wait(&pool.sem);
 
-    pthread_exit(0);
+        pthread_mutex_lock(&pool.mutex);
+        task* current_task = (task*)dequeue(&pool.queue);
+        pthread_mutex_unlock(&pool.mutex);
+
+        if (current_task == NULL) {
+            // Завершение потока
+            pthread_exit(NULL);
+        }
+
+        execute(current_task->function, current_task->data);
+    }
 }
 
-/**
- * Executes the task provided to the thread pool
- */
-void execute(void (*somefunction)(void *p), void *p)
-{
+
+// Executes the task provided to the thread pool
+void execute(void (*somefunction)(void* p), void* p) {
     (*somefunction)(p);
 }
 
-/**
- * Submits work to the pool.
- */
-int pool_submit(void (*somefunction)(void *p), void *p)
-{
-    worktodo.function = somefunction;
-    worktodo.data = p;
+// Submits work to the pool.
+int pool_submit(void (*somefunction)(void* p), void* p) {
+    task* t = (task*)malloc(sizeof(task));
+    t->function = somefunction;
+    t->data = p;
+
+    sem_post(&pool.sem);
+    if (enqueue(&pool.queue, t) != 0) {
+        return 1; // Ошибка добавления задачи в очередь
+    }
 
     return 0;
 }
 
-// initialize the thread pool
-void pool_init(void)
-{
-    pthread_create(&bee, NULL, worker, NULL);
-}
+
 
 // shutdown the thread pool
-void pool_shutdown(void)
-{
-    pthread_join(bee,NULL);
+void pool_shutdown(void) {
+
+    // Ожидание выполнения всех задач
+    while (pool.queue.size > 0) { }
+
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
+        sem_post(&pool.sem); // Уведомить потоки о завершении
+    }
+
+    for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
+        pthread_join(pool.threads[i], NULL); // Уведомить потоки о завершении
+    }
+
+    // Освободить ресурсы
+    free(pool.threads);
+
+    // Уничтожить мьютекс и семафор
+    pthread_mutex_destroy(&pool.mutex);
+    sem_destroy(&pool.sem);
 }
--- program/threadpool.h
+++ program/threadpool.h
@@ -1,6 +1,26 @@
+#pragma once
+#include "queue.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+
 // function prototypes
-void execute(void (*somefunction)(void *p), void *p);
-int pool_submit(void (*somefunction)(void *p), void *p);
-void *worker(void *param);
+void execute(void (*somefunction)(void* p), void* p);
+int pool_submit(void (*somefunction)(void* p), void* p);
+void* worker(void* param);
 void pool_init(void);
 void pool_shutdown(void);
+
+// this represents work that has to be completed by a thread in the pool
+typedef struct {
+    void (*function)(void* p);
+    void* data;
+} task;
+
+typedef struct {
+    pthread_t* threads; // массив потоков
+    queue_t queue; // очередь задачь
+    pthread_mutex_t mutex; // мьютекс для блокировки доступа к очереди
+    sem_t sem; // семафор для уведомления потоков о наличии задач
+} threadpool;
